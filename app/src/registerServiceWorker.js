261c813cc194d2aee0a47528f9a6312d690a940d
Nguyen, Hao | Hao | ECMD <hao.nguyen@rakuten.com>
2018-07-26 14:35:44 +0900

    applied formatter

--- app/src/registerServiceWorker.js
+++ app/src/registerServiceWorker.js
@@ -1,23 +1,15 @@
 // In production, we register a service worker to serve assets from local cache.
-
 // This lets the app load faster on subsequent visits in production, and gives
 // it offline capabilities. However, it also means that developers (and users)
 // will only see deployed updates on the "N+1" visit to a page, since previously
 // cached resources are updated in the background.
-
 // To learn more about the benefits of this model, read https://goo.gl/KwvDNy.
 // This link also includes instructions on opting out of this behavior.
-
-const isLocalhost = Boolean(
-  window.location.hostname === 'localhost' ||
-    // [::1] is the IPv6 localhost address.
-    window.location.hostname === '[::1]' ||
-    // 127.0.0.1/8 is considered localhost for IPv4.
-    window.location.hostname.match(
-      /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-    )
-);
-
+const isLocalhost = Boolean(window.location.hostname === 'localhost' ||
+  // [::1] is the IPv6 localhost address.
+  window.location.hostname === '[::1]' ||
+  // 127.0.0.1/8 is considered localhost for IPv4.
+  window.location.hostname.match(/^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/));
 export default function register() {
   if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
     // The URL constructor is available in all browsers that support SW.
@@ -28,10 +20,8 @@ export default function register() {
       // serve assets; see https://github.com/facebookincubator/create-react-app/issues/2374
       return;
     }
-
     window.addEventListener('load', () => {
       const swUrl = `${process.env.PUBLIC_URL}/service-worker.js`;
-
       if (isLocalhost) {
         // This is running on localhost. Lets check if a service worker still exists or not.
         checkValidServiceWorker(swUrl);
@@ -44,8 +34,7 @@ export default function register() {
 }
 
 function registerValidSW(swUrl) {
-  navigator.serviceWorker
-    .register(swUrl)
+  navigator.serviceWorker.register(swUrl)
     .then(registration => {
       registration.onupdatefound = () => {
         const installingWorker = registration.installing;
@@ -77,15 +66,14 @@ function checkValidServiceWorker(swUrl) {
   fetch(swUrl)
     .then(response => {
       // Ensure service worker exists, and that we really are getting a JS file.
-      if (
-        response.status === 404 ||
-        response.headers.get('content-type').indexOf('javascript') === -1
-      ) {
+      if (response.status === 404 || response.headers.get('content-type')
+        .indexOf('javascript') === -1) {
         // No service worker found. Probably a different app. Reload the page.
         navigator.serviceWorker.ready.then(registration => {
-          registration.unregister().then(() => {
-            window.location.reload();
-          });
+          registration.unregister()
+            .then(() => {
+              window.location.reload();
+            });
         });
       } else {
         // Service worker found. Proceed as normal.
@@ -93,12 +81,9 @@ function checkValidServiceWorker(swUrl) {
       }
     })
     .catch(() => {
-      console.log(
-        'No internet connection found. App is running in offline mode.'
-      );
+      console.log('No internet connection found. App is running in offline mode.');
     });
 }
-
 export function unregister() {
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker.ready.then(registration => {
--- server/src/database.js
+++ server/src/database.js
@@ -1,25 +1,11 @@
-import {MongoClient} from 'mongodb'
-
-const URL = 'mongodb://localhost:27017/chatapp';
-
-
-export default class Database{
-
-	connect(){
-
-
-		return new Promise((resolve, reject) => {
-
-			MongoClient.connect(URL, (err, db) => {
-				
-				return err ? reject(err) : resolve(db);
-
-			});
-
-
-		});
-
-
-
-	}
+import { MongoClient } from 'mongodb'
+const URL = "mongodb://root:admin123@127.0.0.1:27017/chatapp?authSource=admin";
+export default class Database {
+  connect() {
+    return new Promise((resolve, reject) => {
+      MongoClient.connect(URL, (err, db) => {
+        return err ? reject(err) : resolve(db);
+      });
+    });
+  }
 }
